fe3b4c685d551fcbaa551efb2eb7f3a993501594
jaydipnasit-oceanmtech <145960611+jaydipnasit-oceanmtech@users.noreply.github.com>
2024-01-24 12:34:12 +0530

    minore changing
    minore changing

--- lib/src/date/days_view.dart
+++ lib/src/date/days_view.dart
@@ -30,7 +30,7 @@ class DaysView extends StatelessWidget {
   })  : assert(!minDate.isAfter(maxDate), "minDate can't be after maxDate"),
         assert(() {
           if (selectedDate == null) return true;
-          return (selectedDate.isAfter(minDate) || selectedDate.isAtSameMomentAs(minDate)) &&
+          return (selectedDate.isAfter(minDate) || selectedDate.isAtSameMomentAs(minDate)) ||
               (selectedDate.isBefore(maxDate) || selectedDate.isAtSameMomentAs(maxDate));
         }(), "selected date should be in the range of min date & max date");
 
